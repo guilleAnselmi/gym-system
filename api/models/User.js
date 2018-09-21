837807f8677bf5a83b7d5ff723b927f2e65d4989
Guillermo Anselmi <guillermooanselmi@hotmail.com>
2018-09-21 01:31:20 -0300

    Revert "api first setup not working"
    This reverts commit 1c0c7f16a8653f9bbcb3350cbf0d6285271d62d1.

--- api/models/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from 'mongoose'
-
-const UserSchema = new Schema({
-	name: {type: String},
-	password: {type: String}
-})
-
-export default mongoose.model('User', UserSchema)
